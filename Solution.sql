439412a11d80fdb7120aaf1871f9b43e50e9d849
Harun-Malik <100518628+Harun-Malik@users.noreply.github.com>
2022-05-25 17:04:42 +0530

    Update Solution.sql

--- Solution.sql
+++ Solution.sql
@@ -2,13 +2,13 @@ select * from SalesPeople;
 select * from Customers;
 select * from Orders;
 
-select count(Sname) from SalesPeople where Sname like 'a%';         --  ----------------->   Question no-1
+select count(Sname) from SalesPeople where Sname like 'a%';         --  ----------------->   Answer no-1
 
-select * from Orders join SalesPeople on Orders.Snum = SalesPeople.Snum where Amt > 2000;   --  --------------> Question No-2
+select * from Orders join SalesPeople on Orders.Snum = SalesPeople.Snum where Amt > 2000;   --  --------------> Answer No-2
 
-select count(Sname) from SalesPeople where  City = "Newyork";       --  ----------------------> Question No-3
+select count(Sname) from SalesPeople where  City = "Newyork";       --  ----------------------> Answer No-3
 
-select * from SalesPeople where City = "London" || City = "Paris";  --  ---------------------------> Question No-4
+select * from SalesPeople where City = "London" || City = "Paris";  --  ---------------------------> Answer No-4
 
-select Onum, Odate from Orders right join SalesPeople on Orders.Snum = SalesPeople.Snum;   -- ---------> Question No-5
+select Onum, Odate from Orders right join SalesPeople on Orders.Snum = SalesPeople.Snum;   -- ---------> Answer No-5
 
